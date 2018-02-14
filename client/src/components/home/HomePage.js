cc412a7b06a5396f2953c3cf684a34fe065aa850
Rahul Yesantharao <rahul.yesantharao@gmail.com>
2018-02-14 09:56:17 -0500

    Added home background image options

--- client/src/components/home/HomePage.js
+++ client/src/components/home/HomePage.js
@@ -2,7 +2,9 @@ import React from 'react';
 import UserPortal from './UserPortal';
 
 
-// https://unsplash.com/photos/N3o-leQyFsI - add credit
+// https://unsplash.com/photos/N3o-leQyFsI - home
+// https://unsplash.com/photos/dvK_CT1Wg78 - home1
+// https://unsplash.com/photos/pUAM5hPaCRI - home2
 const HomePage = () => {
   return (
     <div className="homePage">
